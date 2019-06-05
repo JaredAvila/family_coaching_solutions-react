3e93eff2fa1a24acf61b99246b728444d67126ec
Jared Avila <jared.c.avila@gmail.com>
2019-06-05 11:02:56 -0700

    landing component done

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,9 +1,15 @@
 import React from "react";
 
+import classes from "./Landing.css";
+
 const Landing = () => {
   return (
-    <div>
-      <h2>Landing Component</h2>
+    <div className={classes.Header}>
+      <div className={classes.Landing} />
+      <div className={classes.Showcase}>
+        <h1 className={classes.Main}>Family Coaching Solutions</h1>
+        <h3 className={classes.Sub}>Building New Beginnings</h3>
+      </div>
     </div>
   );
 };
