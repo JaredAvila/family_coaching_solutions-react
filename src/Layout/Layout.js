c4b425d82019f23ad4728c7dd19a6ca016604dc6
Jared Avila <jared.c.avila@gmail.com>
2019-06-17 10:00:24 -0700

    adding backdrop comp

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -2,10 +2,12 @@ import React, { Fragment } from "react";
 
 import Navbar from "../Components/Navigation/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
+import Backdrop from "../UI/Backdrop";
 
 const Layout = props => {
   return (
     <Fragment>
+      <Backdrop />
       <Navbar />
       <main>{props.children}</main>
       <Footer />
