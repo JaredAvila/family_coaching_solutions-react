3e2bb728313ca6398b63c863927c2dc298a1e084
Jared Avila <jared.c.avila@gmail.com>
2019-06-19 11:02:46 -0700

    services animations

--- src/Components/ServicesComp/ServiceInfo/ServiceInfo.js
+++ src/Components/ServicesComp/ServiceInfo/ServiceInfo.js
@@ -1,68 +1,90 @@
-import React from "react";
-import styles from "./ServiceInfo.css";
+import React, { Component } from "react";
+import { Waypoint } from "react-waypoint";
 
-const ServiceInfo = () => {
-  return (
-    <div className={styles.ServiceInfo}>
-      <h1 className={styles.Title}>
-        Family Coaching Solutions is here to help with many types of issues that
-        exist within the family unit
-      </h1>
-      <div className={styles.Spacer} />
-      <div className={styles.Boxes}>
-        <div className={styles.Code}>
-          <h3>Code of Ethics</h3>
-          <ol className={styles.List}>
-            <li>
-              I will honor Jesus Christ in all that I do; both personally and
-              professionally.
-            </li>
-            <li>
-              I will hold myself accountable to the highest level of character
-              and integrity, and will respect my clients at all times.
-            </li>
-            <li>
-              I will not misrepresent myself, nor my qualifications, expertise,
-              or experience as a Christian Life Coach.
-            </li>
-            <li>
-              I will be compassionate, graceful, and nonjudgmental to all who
-              are seeking Christian Coaching.
-            </li>
-            <li>
-              I will respect people in their own faith journey, and will not
-              impose my own specific religious beliefs.
-            </li>
-            <li>
-              I will honor my Christian values in my professional conduct, and
-              commit to grow both spiritually and intellectually through any
-              combination of personal study, seminars, classes, lectures,
-              workshops, etc.
-            </li>
-            <li>
-              I will respect my client's right to privacy, and will keep all
-              communication confidential, unless otherwise required by law.
-            </li>
-          </ol>
-        </div>
-        <div className={styles.Fees}>
-          <h3>Rates/Fees</h3>
-          <ul className={styles.Rates}>
-            <li>1 hour - $105</li>
-            <li>2 hours - $180</li>
-            <li>3 hours - $250</li>
-            <li>4 hours - $300</li>
-          </ul>
-          <p className={styles.Para}>
-            <em>
-              *** The multiple hourly rates are payable per month in advace, or
-              at the beginning of the first session
-            </em>
-          </p>
+import styles from "./ServiceInfo.css";
+import animate from "../../../UI/Animations.css";
+
+class ServiceInfo extends Component {
+  state = {
+    titleClasses: [styles.Title],
+    codeClasses: [styles.Code],
+    feesClasses: [styles.Fees]
+  };
+
+  codeEnter = () => {
+    this.setState({
+      titleClasses: [styles.Title, animate.FadeInUp],
+      codeClasses: [styles.Code, animate.Flip],
+      feesClasses: [styles.Fees, animate.Flip]
+    });
+  };
+
+  render() {
+    return (
+      <div className={styles.ServiceInfo}>
+        <h1 className={this.state.titleClasses.join(" ")}>
+          Family Coaching Solutions is here to help with many types of issues
+          that exist within the family unit
+        </h1>
+        <div className={styles.Spacer} />
+        <div className={styles.Boxes}>
+          <div className={this.state.codeClasses.join(" ")}>
+            <h3>Code of Ethics</h3>
+            <ol className={styles.List}>
+              <li>
+                I will honor Jesus Christ in all that I do; both personally and
+                professionally.
+              </li>
+              <li>
+                I will hold myself accountable to the highest level of character
+                and integrity, and will respect my clients at all times.
+              </li>
+              <Waypoint onEnter={this.codeEnter}>
+                <li>
+                  I will not misrepresent myself, nor my qualifications,
+                  expertise, or experience as a Christian Life Coach.
+                </li>
+              </Waypoint>
+
+              <li>
+                I will be compassionate, graceful, and nonjudgmental to all who
+                are seeking Christian Coaching.
+              </li>
+              <li>
+                I will respect people in their own faith journey, and will not
+                impose my own specific religious beliefs.
+              </li>
+              <li>
+                I will honor my Christian values in my professional conduct, and
+                commit to grow both spiritually and intellectually through any
+                combination of personal study, seminars, classes, lectures,
+                workshops, etc.
+              </li>
+              <li>
+                I will respect my client's right to privacy, and will keep all
+                communication confidential, unless otherwise required by law.
+              </li>
+            </ol>
+          </div>
+          <div className={this.state.feesClasses.join(" ")}>
+            <h3>Rates/Fees</h3>
+            <ul className={styles.Rates}>
+              <li>1 hour - $105</li>
+              <li>2 hours - $180</li>
+              <li>3 hours - $250</li>
+              <li>4 hours - $300</li>
+            </ul>
+            <p className={styles.Para}>
+              <em>
+                *** The multiple hourly rates are payable per month in advace,
+                or at the beginning of the first session
+              </em>
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default ServiceInfo;
--- src/Components/ServicesComp/ServiceLanding/ServiceLanding.js
+++ src/Components/ServicesComp/ServiceLanding/ServiceLanding.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
+import { Waypoint } from "react-waypoint";
 
 import styles from "./ServiceLanding.css";
+import animate from "../../../UI/Animations.css";
 
-const ServiceLanding = () => {
-  return (
-    <div className={styles.ServiceLanding}>
-      <h1 className={styles.Main}>My Services</h1>
-      <h2 className={styles.Sub}>Building New Beginnings</h2>
-    </div>
-  );
-};
+class ServiceLanding extends Component {
+  state = {
+    mainClasses: [styles.Main],
+    subClasses: [styles.Sub]
+  };
+
+  onEnterHandler = () => {
+    this.setState({
+      mainClasses: [styles.Main, animate.FadeInLeft],
+      subClasses: [styles.Sub, animate.FadeInRight]
+    });
+  };
+
+  render() {
+    return (
+      <Waypoint onEnter={this.onEnterHandler}>
+        <div className={styles.ServiceLanding}>
+          <h1 className={this.state.mainClasses.join(" ")}>My Services</h1>
+          <h2 className={this.state.subClasses.join(" ")}>
+            Building New Beginnings
+          </h2>
+        </div>
+      </Waypoint>
+    );
+  }
+}
 
 export default ServiceLanding;
